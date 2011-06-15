1857ae0df3c43d7c40f87f93344e4a35271a734e
Benjamin Liles <benliles@arch.tamu.edu>
2011-06-15 16:47:28 -0500

    Boilerplate code

---
+++ setup.py
@@ -0,0 +1,29 @@
+from os.path import dirname, join
+
+from setuptools import setup, find_packages
+
+
+
+version = '0.1'
+
+setup(
+    name = 'django-ipauth',
+    version = version,
+    description = "IP Authentication for Django",
+    long_description = open(join(dirname(__file__), 'README')).read() + "\n" + 
+                       open(join(dirname(__file__), 'HISTORY')).read(),
+    classifiers = [
+        "Framework :: Django",
+        "Development Status :: 4 - Beta",
+        #"Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules"],
+    keywords = 'django alumni coa tamu',
+    author = 'TAMU COA ITS',
+    author_email = 'webadmin@arch.tamu.edu',
+    url = 'http://www.arch.tamu.edu',
+    packages = find_packages(),
+    include_package_data = True,
+    zip_safe = False,
+    install_requires = ['setuptools'],
+)
